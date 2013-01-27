ed3bbcf1dc9a398b2b5139562206f64ddbf5cb6d
Randy Coulman <rcoulman@gmail.com>
2013-01-26 22:21:21 -0800

    Add another polling command test.

--- src/test/java/org/jenkinsci/plugins/visualworks_store/StoreSCMTest.java
+++ src/test/java/org/jenkinsci/plugins/visualworks_store/StoreSCMTest.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class StoreSCMTest {
     @Test
@@ -19,5 +20,17 @@ public class StoreSCMTest {
         assertEquals("storeScript -repository Repo -packages Package -versionRegex \\d+ -blessedAtLeast Development",
                 builder.toStringWithQuote());
 
+    }
+
+    @Test
+    public void preparesPollingCommandForMultiplePundles() {
+        List<PundleSpec> pundles = Arrays.asList(new PundleSpec("Package"), new PundleSpec("OtherPackage"), new PundleSpec("Package with Spaces"));
+        StoreSCM scm = new StoreSCM("Repo", pundles, "\\d+", "Development", false, "");
+
+        ArgumentListBuilder builder = scm.preparePollingCommand("storeScript");
+
+        final String commandLine = builder.toStringWithQuote();
+        assertTrue("Command line (" + commandLine + ") doesn't contain expected text",
+                commandLine.contains("-packages Package OtherPackage \"Package with Spaces\""));
     }
 }
